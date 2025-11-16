e528943a260314c457389c80e266f325ab524d97
Navisha Shetkar <126978302+NavishaShetkar@users.noreply.github.com>
2025-11-17 01:01:17 +0530

    Delete Frontend directory

--- Frontend/js/common.js
+++
@@ -1,7 +0,0 @@
-function openLogin() {
-  document.getElementById("loginModal").style.display = "flex";
-}
-
-function closeLogin() {
-  document.getElementById("loginModal").style.display = "none";
-}
